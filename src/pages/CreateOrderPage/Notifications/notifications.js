8d04179f2866fdf0fc00cceeb4e9c66731bf3316
matheus <engmatheusmantini@gmail.com>
2022-10-17 12:00:44 -0300

    Refactor notificaiton messages

--- src/pages/CreateOrderPage/Notifications/notifications.js
+++ src/pages/CreateOrderPage/Notifications/notifications.js
@@ -1,218 +1,97 @@
 import { toast } from "react-toastify";
 
+const toastSettings = {
+  position: "top-right",
+  autoClose: 3000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: "colored",
+};
+
 /* WARN */
 
 export const warnChooseAProduct = () => {
-  toast.warn("Escolha um produto.", {
-    position: "top-right",
-    autoClose: 2000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "colored",
-  });
+  toast.warn("Escolha um produto.", toastSettings);
 };
 
 export const warnQuantityMustBeHigherThanZero = () => {
-  toast.warn("A quantidade precisa ser maior que 0.", {
-    position: "top-right",
-    autoClose: 2000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "colored",
-  });
+  toast.warn("A quantidade precisa ser maior que 0.", toastSettings);
 };
 
 export const warnUnavailableStock = () => {
-  toast.warn(`Este produto está com estoque indisponível.`, {
-    position: "top-right",
-    autoClose: 4000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "colored",
-  });
+  toast.warn(`Este produto está com estoque indisponível.`, toastSettings);
 };
 
 export const warnUnavailableQuantity = (totalAvailable) => {
   toast.warn(
-    `A quantidade informada não está disponível. Escolha um valor menor ou igual a ${totalAvailable}`,
-    {
-      position: "top-right",
-      autoClose: 4000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "colored",
-    }
+    `Quantidade indisponível. Escolha um valor menor ou igual a ${totalAvailable}`,
+    toastSettings
   );
 };
 
 export const warnProductAlreadyAdded = () => {
-  toast.warn("O produto selecionado já foi adicionado ao pedido!", {
-    position: "top-right",
-    autoClose: 4000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "colored",
-  });
+  toast.warn(
+    "O produto selecionado já foi adicionado ao pedido!",
+    toastSettings
+  );
 };
 
 export const warnClientIsRequired = () => {
-  toast.warn("Informe o nome do cliente", {
-    position: "top-right",
-    autoClose: 2000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "colored",
-  });
+  toast.warn("Informe o nome do cliente", toastSettings);
 };
 
 export const warnDeliveryDateIsRequired = () => {
-  toast.warn("Informe a data de entrega", {
-    position: "top-right",
-    autoClose: 2000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "colored",
-  });
+  toast.warn("Informe a data de entrega", toastSettings);
 };
 
 export const warnProductIsRequired = () => {
-  toast.warn("Selecione ao menos um produto.", {
-    position: "top-right",
-    autoClose: 2000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "colored",
-  });
+  toast.warn("Selecione ao menos um produto.", toastSettings);
 };
 
 /* SUCCESS */
 
 export const successProductAdded = () => {
-  toast.success("Produto adicionado com sucesso!", {
-    position: "top-right",
-    autoClose: 2000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "colored",
-  });
+  toast.success("Produto adicionado com sucesso!", toastSettings);
 };
 
 export const successProductEdited = () => {
-  toast.success("Produto editado com sucesso!", {
-    position: "top-right",
-    autoClose: 2000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "colored",
-  });
+  toast.success("Produto editado com sucesso!", toastSettings);
 };
 
 export const successProductDeleted = () => {
-  toast.success("Produto deletado com sucesso!", {
-    position: "top-right",
-    autoClose: 2000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "colored",
-  });
+  toast.success("Produto deletado com sucesso!", toastSettings);
 };
 
 export const successOrderAdded = () => {
-  toast.success("Pedido cadastrado com sucesso!", {
-    position: "top-right",
-    autoClose: 2000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "colored",
-  });
+  toast.success("Pedido cadastrado com sucesso!", toastSettings);
 };
 
 /* ERROR */
 
 export const errorProductCouldNotBeAdded = () => {
-  toast.error("Houve um erro e o produto não pôde ser cadastrado com sucesso", {
-    position: "top-right",
-    autoClose: 2000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "colored",
-  });
+  toast.error(
+    "Houve um erro e o produto não pôde ser cadastrado com sucesso",
+    toastSettings
+  );
 };
 
 export const errorProductCouldNotBeEdited = () => {
-  toast.error("Houve um erro e o produto não pôde ser editado.", {
-    position: "top-right",
-    autoClose: 2000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "colored",
-  });
+  toast.error("Houve um erro e o produto não pôde ser editado.", toastSettings);
 };
 
 export const errorProductCouldNotBeDeleted = () => {
-  toast.error("Houve um erro e o produto não pôde ser deletado.", {
-    position: "top-right",
-    autoClose: 2000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "colored",
-  });
+  toast.error(
+    "Houve um erro e o produto não pôde ser deletado.",
+    toastSettings
+  );
 };
 
 export const errorOrderCouldNotBeAdded = () => {
-  toast.error("Houve um erro e o pedido não pôde ser cadastrado com sucesso!", {
-    position: "top-right",
-    autoClose: 2000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "colored",
-  });
+  toast.error(
+    "Houve um erro e o pedido não pôde ser cadastrado com sucesso!",
+    toastSettings
+  );
 };
